e1c62aeac76f5c5738242c14d690bdde1d760526
Lisa <lfelberg@berkeley.edu>
2017-01-26 18:27:59 -0800

    Adding more notes for the methods

--- python/get_distances.py
+++ python/get_distances.py
@@ -3,29 +3,54 @@ import re
 import numpy as np
 from get_xyz import xyz_reader
 
-BENZENE = 3
+GRAPHENE = 3
 
 def d_pbc(c1, c2, rng):
-    '''Compute distance between points with PBCS'''
-    diff = 0.0
-    ds = 0.0
+    '''Compute distance between points with PBCS
+       c1 and c2 are an array of coordinates, rng is PBC range in x,y,z'''
+    boxl = np.round((c1-c2)/rng) # find what is rounded distance
+    d = c1-c2 - boxl*rng # add that rounded distance to actual dist
+    ds = (d * d).sum(axis=1)
     return ds
 
-def get_coords(filename, tim_xyz, coords, types, dims):
+def get_dist_to_c(tim_xyz, coords, types, dims):
     '''Method to get the distances between graphene and all other particles
        from lammps xyz file'''
-    grap = types == BENZENE
-    other = types != BENZENE
+    grap = types == GRAPHENE; other = types != GRAPHENE; dist_to_C = []
     for i in range(len(tim_xyz)):
         grpC = coords[i,grap,:]
         othC = coords[i,other,:]
-        print("Dims shape", dims.shape)
-        print(grpC.shape, othC.shape)
-        for ot in range(2): #range(len(othC)): #finding closest graphene
-            atomGd = np.repeat(othC[np.newaxis,i,:],len(grpC),axis=0)
-            print(atomGd.shape)
-    
-    return time, dims
+        rng = np.array([dims[i][1]-dims[i][0], dims[i][3]-dims[i][2],
+                        dims[i][5]-dims[i][4]])
+        dis = []
+        for ot in range(len(othC)): #finding closest graphene
+            atomGd = np.repeat(othC[np.newaxis,ot,:],len(grpC),axis=0)
+            dist = d_pbc(grpC, atomGd, rng)
+            clos_C = np.argmin(dist)
+            closestC = np.where(grap)[0][clos_C]
+           #print(clos_C, np.min(dist), closestC)
+            dis.append([np.sqrt(dist[clos_C]), closestC])
+        dist_to_C.append(dis)
+    return dist_to_C
+
+def get_dist_btw_cs():
+    '''Method to get the distance for each graphene carbon to other wall'''
+    return 0.0
+
+def print_dist_to_c(fname, time, dists, types):
+    '''Print distances to carbon wall in xyz like format'''
+    f = open(fname, 'w')
+    for i in range(len(dists)):
+        ctr = 0
+        f.write("{0}\n".format(len(types)))
+        f.write("Atoms. Timestep {0}\n".format(time[i]))
+        for j in range(len(types)):
+            if types[j] == GRAPHENE:
+                clo = j; di = 0.0
+            else:
+                clo = dists[i][ctr][1]; di = dists[i][ctr][0]; ctr += 1
+            f.write("{0} {1} {2:.4f}\n".format(types[j], clo, di))
+    f.close()
 
 
 def get_box_dim(vol_out):
@@ -46,9 +71,10 @@ def main():
     itr = sys.argv[3]
     time, dims = get_box_dim("run"+str(sep)+"_"+str(itr)+".vol")
     tim_xyz, coords, types = xyz_reader(xyzname, time, dims)
-    get_coords("run"+str(sep)+"_"+str(itr)+".dist", tim_xyz, np.array(coords),
-               np.array(types), np.array(dims))
+    dists = get_dist_to_c(tim_xyz, np.array(coords), np.array(types), 
+                          np.array(dims))
 
+    print_dist_to_c("run"+str(sep)+"_"+str(itr)+".dist",tim_xyz,dists,types)
 
 if __name__=="__main__":
     main()
