96c66ab763ea21f27a0131ae5e2c0d4b68d6dd51
Lisa <lfelberg@berkeley.edu>
2017-02-09 19:06:21 -0800

    Adding small script to count the waters on each side of file obtained from lmp data file

---
+++ python/count_wats.py
@@ -0,0 +1,30 @@
+import sys
+
+nlines = int(sys.argv[1])
+fname = "wat.lines"
+
+f = open(fname, 'r').readlines()
+out, old, crr = -1, -1, 0
+
+for line in f:
+    tmp = line.split()
+    ct = tmp[0].split(":")
+    if out == -1:  # This is the first line
+        out = int(ct[0])
+        old = out - 1
+
+    crr = int(ct[0])
+
+    if abs(old-crr) > 330: 
+        brk = crr - old
+        old = crr - 1
+
+    old = crr - 1
+
+lst = crr
+
+brk = float(brk+1)/3.0
+out_all = float(out+(nlines-lst))/3.0
+
+print("{0}\t{1}".format(
+      int(out_all),int(brk)))
