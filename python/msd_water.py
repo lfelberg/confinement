774f34401ccd428fc3663d9541f6a3f9e59dfe7d
Lisa Felberg <lfelberg@berkeley.edu>
2017-07-19 18:10:08 -0700

    Adding in msd for each side

--- python/msd_water.py
+++ python/msd_water.py
@@ -28,8 +28,8 @@ def get_msd(xyz, volC):
     '''Method to get the mean square displacement of water oxygens'''
     nsnaps = len(xyz.atom)-1 # number of snaps in XYZ, remove 0th frame
     # find water oxygen atoms
-    nOX = xyz.get_ct_i(OXY)
     iOX,_ = xyz.get_inner_wat(); oOX,_ = xyz.get_outer_wat()
+    nOX = xyz.get_ct_i(OXY); niO = sum(iOX.astype(int))
     oicd = trans_coords(xyz.atom[1:,iOX],volC.get_rng()[1:]) # don't use 1st snap
     oocd = trans_coords(xyz.atom[1:,oOX],volC.get_rng()[1:]) # don't use 1st snap
 
@@ -37,11 +37,11 @@ def get_msd(xyz, volC):
     ms_mean = np.arange(nsnaps,0,-1.)[:, np.newaxis, np.newaxis]
     for i in range(nsnaps-1):
         ms = d3(oicd[np.newaxis,i], oicd[i+1:])
-        msd[1:nsnaps-i,:sum(iOX.astype(int))] += ms
+        msd[1:nsnaps-i,:niO] += ms
         if sum(oOX.astype(int)) > 0: # will be zero for bulk water
             ms = d3(oocd[np.newaxis,i], oocd[i+1:])
-            msd[1:nsnaps-i,sum(iOX.astype(int)):] += ms
-    return msd/ms_mean
+            msd[1:nsnaps-i,niO:] += ms
+    return msd/ms_mean, niO
 
 def print_msd(msd, timstep, fname):
     '''Print the mean square displacement for 3D, for y-z and for x'''
@@ -67,10 +67,11 @@ def main():
         distance from wall, but only 2D and only for your wall side '''
     xyzname=sys.argv[1]; sep=sys.argv[2]; ln=sys.argv[3]; itr=sys.argv[4]
     nm = str(sep)+"_"+str(ln)+"_"+itr; volC = VolFile("run"+nm+".vol") 
-    xyz_cl = XYZFile(xyzname, volC)
+    xyzC = XYZFile(xyzname, volC)
 
-    msd = get_msd(xyz_cl, volC)
-    print_msd(msd, xyz_cl.time[3]-xyz_cl.time[2], 'wmsd_'+nm+".csv")
+    msd, ninn = get_msd(xyzC, volC)
+    print_msd(msd[:,:ninn],xyzC.time[3]-xyzC.time[2], 'wmsd_'+nm+"_inn.csv")
+    print_msd(msd[:,ninn:],xyzC.time[3]-xyzC.time[2], 'wmsd_'+nm+"_out.csv")
 
 if __name__=="__main__":
     main()
